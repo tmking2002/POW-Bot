8e8b102112256e367411072c113d113cddc4519d
tmking2002 <79228828+tmking2002@users.noreply.github.com>
2022-10-20 00:50:04 -0400

    Delete Player of the Week Bot directory

--- Player of the Week Bot/Model Reference.R
+++
@@ -1,7 +0,0 @@
-stats_byweek <- read_csv("~/Desktop/Player of the Week Bot/Stats by Week.csv")
-
-training <- stats_byweek %>% filter(week_id <= 403)
-
-test_model <- stats_byweek %>% filter(week_id <= 440 & week_id > 403)
-
-log_model <- glm(pow ~ prop_pts + fg_percent + avg_prop_pts + win_pct,data = training)
