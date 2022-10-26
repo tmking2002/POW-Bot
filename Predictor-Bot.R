8a7e787ba1acb8339ffbe71099fc78a29944a768
Tyson King <tysonking@Tysons-MacBook-Air.local>
2022-10-26 17:45:22 -0400

    updated twitter tokens

--- Predictor-Bot.R
+++ Predictor-Bot.R
@@ -57,10 +57,16 @@ send_tweet <- function(weekstart,weekend,conf,eow = FALSE){
     status = paste0(conf,"ern Conference: ",format(as.Date(weekstart),"%b %d")," through ",format(as.Date(weekend),"%b %d")," Final Predictions")
   }
   
+  #Use for manual posting
+  #token = twitter_token
+  
+  #Use for actual bot
+  token = POWbot_token
+  
   post_tweet(status = status,
              media = "POW Table.png",
              media_alt_text = paste0("Table showing the top 5 favorites for the ",conf,"ern Conference player of the week, ",format(as.Date(weekstart),"%b %d, %Y")," through ",format(as.Date(weekend),"%b %d, %Y")),
-             token = twitter_token)
+             token = token)
 }
 
 send_tweet(weekstart,weekend,"West")
